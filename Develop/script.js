3299574f2bd8dc85042c60efac2e0ab4a093081b
AndreaB <128647787+andreaber23@users.noreply.github.com>
2023-04-19 18:26:55 -0400

    random password generator is working when the button is clicked. The function writePassword was updated with the new variable that was created, PasswordCriteria

--- Develop/script.js
+++ Develop/script.js
@@ -7,15 +7,20 @@ var upperCase=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","
 var numbers=["1","2","3","4","5","6","7","8","9","0"];
 var specialCharacters=["!","@","#","$","%","&","*","(",")","+","[","]"];
 
+
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  function writePassword() {
+    var confirmedCriteria = passwordCriteria();
+    var passwordText = document.querySelector("#password");
+    
+      if (confirmedCriteria){
+        var password = generatePassword();
+        
+        passwordText.value = password;
+  }
+}
 
-  passwordText.value = password;
-
-
-  function generatePassword() {
+function generatePassword() {
     var passwordCreated = ""
         for (var i=0; i< passwordsLength; i++) {
        var randomPassword = Math.floor(Math.random() * randomCharacters.length);
@@ -24,20 +29,57 @@ function writePassword() {
     return passwordCreated;
   }
 
-  function passwordCriteria() {
-    passwordsLength= +prompt("How many characters would you like your password to have?");
-  if (passwordsLength<8 || passwordsLength>128 || passwordsLength===" ") {
-      alert("Re-enter valid entry. It has to be between 8 and 128 characters long");
-      return false
-  
-  
-  }
 
 
 
+
+
+  // 1. Presented with password criteria
+  //  a.length of password 8<128
+function passwordCriteria() {
+  passwordsLength= +prompt("How many characters would you like your password to have?");
+if (passwordsLength<8 || passwordsLength>128 || passwordsLength===" ") {
+    alert("Re-enter valid entry. It has to be between 8 and 128 characters long");
+    return false
 
 
 }
+  
+
+
+
+
+  //  b.confirm if lower case or not
+  if (confirm("Click OK to confirm lowercase letters")) {
+    randomCharacters=(randomCharacters.concat(lowerCase))
+      }
+  //  c.confirm if upper or lower case
+  if (confirm("Click OK to confirm uppercase letters")) {
+    randomCharacters=(randomCharacters.concat(upperCase))
+    }
+  //  d. numeric characters or not
+  if (confirm("Click OK to confirm numbers in your password")) {
+    randomCharacters=(randomCharacters.concat(numbers))
+   }
+  //  e. special characters or no
+  if (confirm("Click OK to confirm special characters ")) {
+    randomCharacters=(randomCharacters.concat(specialCharacters))
+   }
+
+   return true 
+}
+  // 2. Validate input and at least 1 character type included
+  // 3. Generate password based on criteria
+  // 4. Password is displayed 
+
+
+
+  
+
+
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
