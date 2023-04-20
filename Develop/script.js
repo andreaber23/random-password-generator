f153d2d66468ba03a6a5cf12de27b9d2f15ebbd2
AndreaB <128647787+andreaber23@users.noreply.github.com>
2023-04-19 21:41:42 -0400

    added isNan to if statement in function passwordCriteria to fix the issue if the user doesn't input a number

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var passwordsLength = 8;
+var passwordsLength = "";
 var randomCharacters= [];
 var lowerCase=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperCase=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
@@ -29,25 +29,14 @@ function generatePassword() {
     return passwordCreated;
   }
 
-
-
-
-
-
   // 1. Presented with password criteria
   //  a.length of password 8<128
 function passwordCriteria() {
-  passwordsLength= +prompt("How many characters would you like your password to have?");
-if (passwordsLength<8 || passwordsLength>128 || passwordsLength===" ") {
+  passwordsLength= prompt("How many characters would you like your password to have?");
+if (isNaN(passwordsLength) || passwordsLength<8 || passwordsLength>128 || passwordsLength===" " ) {
     alert("Re-enter valid entry. It has to be between 8 and 128 characters long");
     return false
-
-
 }
-  
-
-
-
 
   //  b.confirm if lower case or not
   if (confirm("Click OK to confirm lowercase letters")) {
@@ -68,18 +57,6 @@ if (passwordsLength<8 || passwordsLength>128 || passwordsLength===" ") {
 
    return true 
 }
-  // 2. Validate input and at least 1 character type included
-  // 3. Generate password based on criteria
-  // 4. Password is displayed 
-
-
-
-  
-
-
-
-
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
